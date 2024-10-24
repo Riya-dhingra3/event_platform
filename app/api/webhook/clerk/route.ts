b2cfdde9836d7a05a994266793ad9d238efd5362
riya.dhingra <riyadhingra248@gmail.com>
2024-10-24 13:31:20 +0530

    usererror32

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -67,8 +67,8 @@ export async function POST(req: Request) {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name || '',
-      lastName: last_name || '',
+      firstName: first_name || 'Hii',
+      lastName: last_name || 'Hello',
       photo: image_url,
     };
 
